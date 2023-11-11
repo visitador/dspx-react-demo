e666bb84d91817d0346aa276a7994d28ffe9d4fe
Yaroslav Golubev <my3blkant50@gmail.com>
2023-11-11 17:32:29 +0500

    setup github pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: `/dspx-react-demo`,
   siteMetadata: {
     title: `DSPX React Demo`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://visitador.github.io/dspx-react-demo/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
